ab82d0e0213c21660c43df258225ebf78de44473
Adam Sturrock <adam@adamsturrock.co.uk>
2013-01-08 10:42:00 +0000

    Set equal heights
    Sets image slider items heights to be equal.

--- views/admin/entries.php
+++ views/admin/entries.php
@@ -11,21 +11,23 @@
 						<img src="<?php echo site_url('files/thumb/'.$entry['image']['id'].'/300/77/fit'); ?>" width="300" height="77">
 						<span>
 							<div class="clear"><?php echo $entry['title']; ?></div>
-							<?php if ($entry['status']['key'] == 'live'): ?>
-								<a class="btn green confirm" title="Are you sure you want to set this image to draft? It will no longer appear on the site!" href="<?php echo site_url('admin/slider/draft/'.$entry['id']); ?>">
-									<span>Live</span>
+							<div class="buttons">
+								<?php if ($entry['status']['key'] == 'live'): ?>
+									<a class="btn green confirm" title="Are you sure you want to set this image to draft? It will no longer appear on the site!" href="<?php echo site_url('admin/slider/draft/'.$entry['id']); ?>">
+										<span>Live</span>
+									</a>
+								<?php else: ?>
+									<a class="btn orange confirm" title="Are you sure you want to set this image to live? It will be visible on the site!" href="<?php echo site_url('admin/slider/live/'.$entry['id']); ?>">
+										<span>Draft</span>
+									</a>
+								<?php endif; ?>
+								<a class="btn blue" href="<?php echo site_url('admin/slider/edit/'.$entry['id']); ?>">
+									<span>Edit</span>
 								</a>
-							<?php else: ?>
-								<a class="btn orange confirm" title="Are you sure you want to set this image to live? It will be visible on the site!" href="<?php echo site_url('admin/slider/live/'.$entry['id']); ?>">
-									<span>Draft</span>
+								<a class="btn red confirm" href="<?php echo site_url('admin/slider/delete/'.$entry['id']); ?>">
+									<span>Delete</span>
 								</a>
-							<?php endif; ?>
-							<a class="btn blue" href="<?php echo site_url('admin/slider/edit/'.$entry['id']); ?>">
-								<span>Edit</span>
-							</a>
-							<a class="btn red confirm" href="<?php echo site_url('admin/slider/delete/'.$entry['id']); ?>">
-								<span>Delete</span>
-							</a>
+							</div>
 						</span>
 					</div>
 				<?php endforeach; ?>
